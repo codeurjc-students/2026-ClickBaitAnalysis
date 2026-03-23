a4b072c5379525cca5ddde2c417e5d9c2c6ca834
Gonzalo <g.garciac.2022@alumnos.urjc.es>
2026-03-23 18:51:14 +0100

    fix: Used API on tools and return serializable types from get_alerts and get_news_this_week

--- backend/tools/the_guardian_tool.py
+++ backend/tools/the_guardian_tool.py
@@ -7,13 +7,16 @@ News API for The Guardian
 import json
 
 from mcp.server.fastmcp import FastMCP
-from backend.api.the_guardian_api import get_news_this_week_call
+from backend.api.the_guardian_api import GuardianAPI
 from backend.models import ToolResult
 
 
 #TODO: Implementar validador? Acaso hay campos cerrados
 #TODO: Esta tool podría inferir! (Preguntar que temas quiere)
 def register(mcp: FastMCP):
+    
+    api = GuardianAPI()
+    
     @mcp.tool()
     async def get_news_this_week(topic: str) -> str:
         
@@ -22,7 +25,9 @@ def register(mcp: FastMCP):
         Args:
             topic: the topic to search for
         """
-        response = await get_news_this_week_call(topic)
+        response = await api.get_news_this_week_call(topic)
+        if not response.has_content():
+            return response.error or "Error fetching news"
         return json.dumps(response.data)
         
         
